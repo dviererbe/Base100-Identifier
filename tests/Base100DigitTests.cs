8ea3915ac6c49abbb0672cd06f884b6dba6b5532
Dominik Viererbe <me@dviererbe.de>
2021-10-27 05:52:18 +0200

    feat: implement `ToString()` for `Base100Digit`
    - implement `ToString()`
    - implement `ToString(string?)`
    - implement `ToString(IFormatProvider?)`
    - implement `ToString(string?, IFormatProvider?)` [implements `IFormattable`]
    - add unit tests for the implementations

--- tests/Base100DigitTests.cs
+++ tests/Base100DigitTests.cs
@@ -2,6 +2,7 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 using Xunit;
+using System.Globalization;
 
 namespace Base100Identifier.UnitTests
 {
@@ -44,10 +45,9 @@ namespace Base100Identifier.UnitTests
 
         [Theory]
         [MemberData(nameof(InvalidBase100DigitValues))]
-        public void Constructor_ShouldThrow_ArgumentOutOfRangeException_WhenValueIsLargerThan99(in byte value)
+        public void Constructor_ShouldThrow_ArgumentOutOfRangeException_WhenValueIsLargerThan99(byte value)
         {
-            byte valueCopy = value; //needed because lambda-expressions can't contain in-Parameters
-            Assert.Throws<ArgumentOutOfRangeException>(() => new Base100Digit(valueCopy));
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Base100Digit(value));
         }
 
         [Theory]
@@ -56,6 +56,174 @@ namespace Base100Identifier.UnitTests
         {
             Base100Digit digit = new Base100Digit(value);
             Assert.Equal(expected: value, actual: digit.GetHashCode());
+        }
+
+        [Theory]
+        [InlineData((byte)00, "00")]
+        [InlineData((byte)09, "09")]
+        [InlineData((byte)10, "10")]
+        [InlineData((byte)99, "99")]
+        public void ToString_ShouldReturn_ExpectedStringRepresentation(in byte value, string expectedStringRepresentation)
+        {
+            Base100Digit digit = new(value);
+            Assert.Equal(
+                expected: expectedStringRepresentation, 
+                actual: digit.ToString());
+        }
+
+        [Theory]
+        [InlineData((byte)00, null, "00")]
+        [InlineData((byte)00, "", "00")]
+        [InlineData((byte)09, null, "09")]
+        [InlineData((byte)09, "", "09")]
+        [InlineData((byte)85, null, "85")]
+        [InlineData((byte)85, "", "85")]
+        [InlineData((byte)85, "C3", "$85.000")]
+        [InlineData((byte)85, "D4", "0085")]
+        [InlineData((byte)85, "e1", "8.5e+001")]
+        [InlineData((byte)85, "E2", "8.50E+001")]
+        [InlineData((byte)85, "F1", "85.0")]
+        [InlineData((byte)85, "G", "85")]
+        [InlineData((byte)85, "N1", "85.0")]
+        [InlineData((byte)85, "P0", "8,500%")]
+        [InlineData((byte)85, "X4", "0055")]
+        [InlineData((byte)85, "0000.0000", "0085.0000")]
+        public void ToString_WithSpecificFormat_ShouldReturn_ExpectedStringRepresentation(in byte value, string? format, string expectedStringRepresentation)
+        {
+            CultureInfo.CurrentCulture = new CultureInfo("en-US");
+            Base100Digit digit = new(value);
+
+            Assert.Equal(
+                expected: expectedStringRepresentation, 
+                actual: digit.ToString(format));
+        }
+
+        [Theory]
+        [InlineData((byte)00, null, "00")]
+        [InlineData((byte)00, "en-US", "00")]
+        [InlineData((byte)00, "fr-FR", "00")]
+        [InlineData((byte)00, "de-DE", "00")]
+        [InlineData((byte)00, "es-ES", "00")]
+        [InlineData((byte)01, null, "01")]
+        [InlineData((byte)01, "en-US", "01")]
+        [InlineData((byte)01, "fr-FR", "01")]
+        [InlineData((byte)01, "de-DE", "01")]
+        [InlineData((byte)01, "es-ES", "01")]
+        [InlineData((byte)14, null, "14")]
+        [InlineData((byte)14, "en-US", "14")]
+        [InlineData((byte)14, "fr-FR", "14")]
+        [InlineData((byte)14, "de-DE", "14")]
+        [InlineData((byte)14, "es-ES", "14")]
+        [InlineData((byte)42, null, "42")]
+        [InlineData((byte)42, "en-US", "42")]
+        [InlineData((byte)42, "fr-FR", "42")]
+        [InlineData((byte)42, "de-DE", "42")]
+        [InlineData((byte)42, "es-ES", "42")]
+        [InlineData((byte)99, null, "99")]
+        [InlineData((byte)99, "en-US", "99")]
+        [InlineData((byte)99, "fr-FR", "99")]
+        [InlineData((byte)99, "de-DE", "99")]
+        [InlineData((byte)99, "es-ES", "99")]
+        public void ToString_WithSpecificFormatProvider_ShouldReturn_ExpectedStringRepresentation(in byte value, string? culture, string expectedStringRepresentation)
+        {
+            CultureInfo? cultureInfo = culture switch {
+                not null => new CultureInfo(culture),
+                _ => null 
+            };
+        
+            Base100Digit digit = new(value);
+
+            Assert.Equal(
+                expected: expectedStringRepresentation, 
+                actual: digit.ToString(cultureInfo));
+        }
+
+        [Theory]
+        [InlineData((byte)00, null, null, "00")]
+        [InlineData((byte)06, null, null, "06")]
+        [InlineData((byte)14, null, null, "14")]
+        [InlineData((byte)42, null, null, "42")]
+        [InlineData((byte)99, null, null, "99")]
+        [InlineData((byte)00, "", null, "00")]
+        [InlineData((byte)06, "", null, "06")]
+        [InlineData((byte)14, "", null, "14")]
+        [InlineData((byte)42, "", null, "42")]
+        [InlineData((byte)99, "", null, "99")]
+        [InlineData((byte)00, null, "en-US", "00")]
+        [InlineData((byte)06, null, "en-US", "06")]
+        [InlineData((byte)14, null, "en-US", "14")]
+        [InlineData((byte)42, null, "en-US", "42")]
+        [InlineData((byte)99, null, "en-US", "99")]
+        [InlineData((byte)00, "", "en-US", "00")]
+        [InlineData((byte)06, "", "en-US", "06")]
+        [InlineData((byte)14, "", "en-US", "14")]
+        [InlineData((byte)42, "", "en-US", "42")]
+        [InlineData((byte)99, "", "en-US", "99")]
+        [InlineData((byte)00, null, "de-DE", "00")]
+        [InlineData((byte)06, null, "de-DE", "06")]
+        [InlineData((byte)14, null, "de-DE", "14")]
+        [InlineData((byte)42, null, "de-DE", "42")]
+        [InlineData((byte)99, null, "de-DE", "99")]
+        [InlineData((byte)00, "", "de-DE", "00")]
+        [InlineData((byte)06, "", "de-DE", "06")]
+        [InlineData((byte)14, "", "de-DE", "14")]
+        [InlineData((byte)42, "", "de-DE", "42")]
+        [InlineData((byte)99, "", "de-DE", "99")]
+        [InlineData((byte)85, "C3", "en-US", "$85.000")]
+        [InlineData((byte)85, "D4", "en-US", "0085")]
+        [InlineData((byte)85, "e1", "en-US", "8.5e+001")]
+        [InlineData((byte)85, "E2", "en-US", "8.50E+001")]
+        [InlineData((byte)85, "F1", "en-US", "85.0")]
+        [InlineData((byte)85, "G", "en-US", "85")]
+        [InlineData((byte)85, "N1", "en-US", "85.0")]
+        [InlineData((byte)85, "P0", "en-US", "8,500%")]
+        [InlineData((byte)85, "X4", "en-US", "0055")]
+        [InlineData((byte)85, "0000.0000", "en-US", "0085.0000")]
+        [InlineData((byte)85, "C3", "de-DE", "85,000 €")]
+        [InlineData((byte)85, "D4", "de-DE", "0085")]
+        [InlineData((byte)85, "e1", "de-DE", "8,5e+001")]
+        [InlineData((byte)85, "E2", "de-DE", "8,50E+001")]
+        [InlineData((byte)85, "F1", "de-DE", "85,0")]
+        [InlineData((byte)85, "G", "de-DE", "85")]
+        [InlineData((byte)85, "N1", "de-DE", "85,0")]
+        [InlineData((byte)85, "P0", "de-DE", "8.500 %")]
+        [InlineData((byte)85, "X4", "de-DE", "0055")]
+        [InlineData((byte)85, "0000.0000", "de-DE", "0085,0000")]
+        public void ToString_WithSpecificFormatAndFormatProvider_ShouldReturn_ExpectedStringRepresentation(in byte value, string? format, string? culture, string expectedStringRepresentation)
+        {
+            CultureInfo? cultureInfo = culture switch {
+                not null => new CultureInfo(culture),
+                _ => null 
+            };
+        
+            Base100Digit digit = new(value);
+
+            Assert.Equal(
+                expected: expectedStringRepresentation, 
+                actual: digit.ToString(format, cultureInfo));
+        }
+
+        [Theory]
+        [InlineData((byte)00, "r")]
+        [InlineData((byte)00, "R")]
+        [InlineData((byte)06, "r")]
+        [InlineData((byte)06, "R")]
+        [InlineData((byte)14, "r")]
+        [InlineData((byte)14, "R")]
+        [InlineData((byte)42, "r")]
+        [InlineData((byte)42, "R")]
+        [InlineData((byte)99, "r")]
+        [InlineData((byte)99, "R")]
+        public void ToString_WithInvalidFormat_ShouldThrow_FormatException(in byte value, string invalidFormat)
+        {
+            Base100Digit digit = new(value);
+
+            Assert.Throws<FormatException>(() => digit.ToString(invalidFormat));
+            Assert.Throws<FormatException>(() => digit.ToString(invalidFormat, formatProvider: null));
+            Assert.Throws<FormatException>(() => digit.ToString(invalidFormat, formatProvider: new CultureInfo("en-US")));
+            Assert.Throws<FormatException>(() => digit.ToString(invalidFormat, formatProvider: new CultureInfo("fr-FR")));
+            Assert.Throws<FormatException>(() => digit.ToString(invalidFormat, formatProvider: new CultureInfo("de-DE")));
+            Assert.Throws<FormatException>(() => digit.ToString(invalidFormat, formatProvider: new CultureInfo("es-ES")));
         }
     }
 }
